552d084d8717c647d8ecb71c9adf2228c9620c2e
Mai Houa Vue <mhvue4@gmail.com>
2020-12-06 11:14:41 -0600

    added bootstrap css and js

---
+++ app.js
@@ -0,0 +1,10 @@
+const dinosaurFacts = [
+    "Dinosaurs ruled the Earth for over 160 million years",
+    "A person who studies dinosaurs is known as a paleontologist",
+    "Birds descended from a type of dinosaurs",
+    "The name dinosaur means 'frighteningly big lizard",
+    "Dinosaur footprints have been found all over the world and give scientists lots of clues about what made them!",
+
+]
+
+//https://www.sciencekids.co.nz/sciencefacts/dinosaurs.html
