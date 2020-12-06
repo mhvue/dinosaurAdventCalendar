8b506b0891dcfd04a881184778e6288bbbe3c137
Mai Houa Vue <mhvue4@gmail.com>
2020-12-06 12:43:22 -0600

    add the numbered days of the week for December 2020

--- app.js
+++ app.js
@@ -8,3 +8,45 @@ const dinosaurFacts = [
 ]
 
 //https://www.sciencekids.co.nz/sciencefacts/dinosaurs.html
+
+const date = new Date();
+console.log(date)
+
+let days = "";
+
+//1st week
+for(let i = -1; i <= 5; i++){
+    days = i;
+    //hide -1 and 0
+    if(i == -1 || i == 0){
+       $("#numberDays").append("<td>"+ days + "</td>").css("visibility:hidden")
+    }
+    else{
+        $("#numberDays").append("<td>"+ days + "</td>");
+
+    }
+}
+
+//2nd week
+for(let i = 6; i <= 12; i++){
+    days = i;
+    $("#numberDays2").append("<td>"+ days + "</td>");
+}
+//3rd week
+for(let i = 13; i <= 19; i++){
+    days = i;
+    $("#numberDays3").append("<td>"+ days + "</td>");
+}
+
+//4th week
+for(let i = 20; i <= 26; i++){
+    days = i;
+    $("#numberDays4").append("<td>"+ days + "</td>");
+}
+
+//5th week
+for(let i = 27; i <= 31; i++){
+    days = i;
+    $("#numberDays5").append("<td>"+ days + "</td>");
+}
+
