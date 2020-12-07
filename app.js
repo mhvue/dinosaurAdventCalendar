0cc94b7a62e288fbcaafc2250f80258dfc1262de
Mai Houa Vue <mhvue4@gmail.com>
2020-12-07 14:00:47 -0600

    added more information in dinosaurFacts arry and more imgs in the imgs object. comments some sections to test

--- app.js
+++ app.js
@@ -7,42 +7,45 @@ const imgs = {
     dino6: "<img src='./images/skeleton-triceratops.jpg'>",
     dino7: "<img src='./images/fossils-heterodontosaurus.jpg'>",
     dino8: "<img src='./images/Heterodontosaurus.jpg'>",
+    dino9: "<img src='./images/Stego.jpg'>",
+    dino10: "<img src='./images/Saltopus.jpg'>",
+    dino11: "<img src='./images/Dreadnoughtus.jpg'>",
+    dino12: "<img src='./images/Sauropods.jpg'>",
 }
 
 const dinosaurFacts = [
-    "Heterodontonsaurus fossil" + imgs.dino7, //testing img here
+    // "Triceratops had nose horns they used for defence." + imgs.dino1 + "<br>Picture and info by https://www.kids-dinosaurs.com/dinosaurs-triceratops.html",
     "First dinosaur bones discovered 1677 by Robert Plot -https://www.discovery.com/science/First-Dinosaur-Fossil-Name",
     "Dinosaurs ruled the Earth for over 160 million years",
     "A person who studies dinosaurs is known as a paleontologist",
     "Birds have ancestors of dinosaurs",
     "The name dinosaur means 'frighteningly big lizard",
     "Dinosaur footprints have been found all over the world and give scientists lots of clues about what made them!",
-    "Dreatnoughtus was the largest dinosaur.",
+    "Dreadnoughtus was the largest dinosaur." + imgs.dino11 + "<br>Picture by: By ArcaneHalveKnot - https://commons.wikimedia.org/wiki/File:Dreadnoughtus_Published_Reconstruction.jpg",
     "Velociraptors is pronounce 'vell-os-eeraptor' ",
-    "Triceratops had nose horns they used for defence." + imgs.dino1 + "-https://www.kids-dinosaurs.com/dinosaurs-triceratops.html",
+    "Triceratops had nose horns they used for defence." + imgs.dino1 + "<br>Picture and info by https://www.kids-dinosaurs.com/dinosaurs-triceratops.html",
     "T. Rex had quite a large brain for a dinosaur. This would have helped it hunt better!" +
     "Large parts of its brain helped its vision and smell-it may have even had telescopic vision.",
-    "Theropods - these are the meat eating dinosaurs.",
-    "Sauropods have long necks and lon tails.",
+    "Theropods means beast foot. It is a group of dinosaurs that has three toes for each leg and very short arms. For example: T-Rex",
+    "Sauropods are a group of dinosaurs that have long necks, long tails, and they walked on all four feet."+ imgs.dino12 + "<br>Picture by: By DiBgd, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=2443100",
     "Apatosaurus lived around 150 million years ago",
     "First dinosaur bones discovered 1677 by Robert Plot -https://www.discovery.com/science/First-Dinosaur-Fossil-Name",
     "Dinosaurs extinct about 65 million years ago -https://www.kids-dinosaurs.com/dinosaur-extinction.html",
-    "The smallest meat eating dinosaurs are Hesperonychus." + imgs.dino2 + "Picture By Nobu Tamura (http://spinops.blogspot.com)",
-    "Allosaurus weight 4 tons." + imgs.dino3 + "By Fred Wierum - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=47577505",
-    "Brachiosaurus name means 'arm-reptiles'" + imgs.dino4 + "-Info https://www.kids-dinosaurs.com/brachiosaurus.html. Picture: By Богданов dmitrchel@mail.ru",
-    "Bone of a Ceratosaurus." + imgs.dino5,
-    "Bone of a Triceratops" + imgs.dino6,
-    "Joke: What did the dinosaur say when the volacano exploded?.....Answer: Have a Lava-ly day!",
-    "Heterodontonsaurus" + imgs.dino8 + "Picture: By FunkMonk (Michael B. H.) - Own work, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=44921379",
-    "Heterodontonsaurus fossil" + imgs.dino7,
-    "fact24",
-    "fact25",
-    "fact26",
-    "fact27",
-    "fact28",
-    "fact29",
-    "fact30",
-    "fact31",
+    "The smallest meat eating dinosaurs are Hesperonychus." + imgs.dino2 + "<br>Picture By Nobu Tamura (http://spinops.blogspot.com)",
+    "Allosaurus weight 4 tons." + imgs.dino3 + "<br>By Fred Wierum - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=47577505",
+    "Brachiosaurus name means 'arm-reptiles'" + imgs.dino4 + "<br>-Info https://www.kids-dinosaurs.com/brachiosaurus.html. Picture: By Богданов dmitrchel@mail.ru",
+    "Bone of a Ceratosaurus. Most of the time, they ate fish and crocodiles. Makes sense as they are good swimmers!" + imgs.dino5,
+    "Bone of a Triceratops. They have a total of 985 bones!" + imgs.dino6,
+    "Joke: What did the dinosaur say when the volacano exploded?....." + "<br> Answer: Have a Lava-ly day!",
+    "Heterodontonsaurus have flexible tail." + imgs.dino8 + "<br>Picture: By FunkMonk (Michael B. H.) - Own work, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=44921379",
+    "Fossils are remains of ancient animals. It helps us learn about them. Here is the Heterodontonsaurus fossil" + imgs.dino7,
+    "Herbivores means eating plants. Carnivore meants eating plants. Omnivore means eating meat and plants. Most dinosaures were herbivore.",
+    "Dinosaurs did not live in oceans, lakes or rivers. Most can paddle through water.",
+    "The smallest dinosuar is Lesothosaurus, which is the size of big chickens. -https://www.scholastic.com/teachers/articles/teaching-content/dinosaurs-smallest-largest/",
+    "T. Rex was 12 feet tall. -https://www.scholastic.com/teachers/articles/teaching-content/dinosaurs-smallest-largest/",
+    "Did dinosaur lay eggs?" + "<br> Yes they did!",
+    "The Stegosaurus had a brain the size of a walnut." + imgs.dino9 + "<br>Info by https://www.thedinosaurmuseum.com/dino-facts. Picture by: By Charles R. Knight. Published in Gilmore, C.W. (1914).",
+    "The oldest dinosaur is Saltopus. It lived for 245 million years ago." + imgs.dino10 + "<br>Picture by By Conty - Own work, Public Domain, https://commons.wikimedia.org/w/index.php?curid=6812666",
 ]
 
 //fact rec'd from: https://www.sciencekids.co.nz/sciencefacts/dinosaurs.html
@@ -61,7 +64,7 @@ $("h2").html("December " + year)
 //generating dates
 for(let i = -1; i <= 33; i++){
     getDays = i;
-    tdData = $("<td>").html(getDays).attr("id",`12/${i}/${year}`); 
+    tdData = $("<td>").html(getDays).attr("id",`12/${i}/${year}`).attr("data-num",i); 
 
     //1st week
     if(i == -1 || i == 0){
@@ -99,32 +102,39 @@ $("td").on("click",function(){
     //$this is grabbing the id per day 
     const selectDate = $(this).attr("id");
     const getNum= $(this).html();
+    console.log(getNum)
     const currentDate = moment();
     const getD = currentDate.format("D");
   
+    $(".msgModal").modal();
+
+    //show match getNum to match index-1
+    $("#dinoFactHolder").html(dinosaurFacts[getNum-1]);
+
     //check if what is clicked matched today's date
     //current date 
-    if(selectDate == currentDate.format("MM/D/YYYY")){
-        $(".msgModal").modal();
+    // if(selectDate == currentDate.format("MM/D/YYYY")){
+    //     $(".msgModal").modal();
 
-        //show match getNum to match index-1
-        $("#dinoFactHolder").html(dinosaurFacts[getNum-1]);
-    }
-    // //previous date
-    // //whatever is clicked on, have to check to see if less than current num
-    else if(parseInt(getNum) < parseInt(getD)){
-        $(".msgModal").modal();
-        $("#dinoFactHolder").html(dinosaurFacts[getNum-1]);
-    }
+    //     //show match getNum to match index-1
+    //     $("#dinoFactHolder").html(dinosaurFacts[getNum-1]);
+    // }
+    // // //previous date
+    // // //whatever is clicked on, have to check to see if less than current num
+    // else if(parseInt(getNum) < parseInt(getD)){
+    //     $(".msgModal").modal();
+    //     $("#dinoFactHolder").html(dinosaurFacts[getNum-1]);
+    // }
+
     // //for areas with no dates/num on it  
-    else if(getNum == ""){
-        $(".msgModal").modal();
-        $("#dinoFactHolder").html("<p>No date here</p>");
-    }
-    //future dates = cannot show 
-    else{
-        $(".msgModal").modal();
-        $("#dinoFactHolder").html("<p>Not time to see this fact yet</p>");
-    }
+    // else if(getNum == ""){
+    //     $(".msgModal").modal();
+    //     $("#dinoFactHolder").html("<p>No date here</p>");
+    // }
+    // //future dates = cannot show 
+    // else{
+    //     $(".msgModal").modal();
+    //     $("#dinoFactHolder").html("<p>Not time to see this fact yet</p>");
+    // }
 
 });
