5d0b8ac2aff5de5a60b7c21c187ad337515db0b2
Mai Houa Vue <mhvue4@gmail.com>
2020-12-07 00:30:58 -0600

    added more dinosaur pictures, credit to website obtained of info and pictures in array

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 const imgs = {
-    dino1: "<img src='./images/triceratops.jpg'>"
+    dino1: "<img src='./images/triceratops.jpg'>",
+    dino2: "<img src='./images/Hesperonychus.jpg'>",
+    dino3: "<img src='./images/Allosaurus.jpg'>",
+    dino4: "<img src='./images/Brachiosaurus.jpg'>",
+    dino5: "<img src='./images/skeleton-ceratosaurus.jpg'>",
+    dino6: "<img src='./images/skeleton-triceratops.jpg'>",
+    dino7: "<img src='./images/fossils-heterodontosaurus.jpg'>",
+    dino8: "<img src='./images/Heterodontosaurus.jpg'>",
 }
 
 const dinosaurFacts = [
-     imgs.dino1,
+    "Heterodontonsaurus fossil" + imgs.dino7,
+    "First dinosaur bones discovered 1677 by Robert Plot -https://www.discovery.com/science/First-Dinosaur-Fossil-Name",
     "Dinosaurs ruled the Earth for over 160 million years",
     "A person who studies dinosaurs is known as a paleontologist",
-    "Birds descended from a type of dinosaurs",
+    "Birds have ancestors of dinosaurs",
     "The name dinosaur means 'frighteningly big lizard",
     "Dinosaur footprints have been found all over the world and give scientists lots of clues about what made them!",
     "Dreatnoughtus was the largest dinosaur.",
     "Velociraptors is pronounce 'vell-os-eeraptor' ",
-    "Triceratops had nose horns they used for defence.",
+    "Triceratops had nose horns they used for defence." + imgs.dino1 + "-https://www.kids-dinosaurs.com/dinosaurs-triceratops.html",
     "T. Rex had quite a large brain for a dinosaur. This would have helped it hunt better!" +
     "Large parts of its brain helped its vision and smell-it may have even had telescopic vision.",
     "Theropods - these are the meat eating dinosaurs.",
     "Sauropods have long necks and lon tails.",
-     "Apatosaurus lived around 150 million years ago",
-    "fact13",
-    "fact14",
-    "fact15",
-    "fact16",
-    "fact17",
-    "fact18",
-    "fact19",
-    "fact20",
-    "fact21",
-    "fact22",
-    "fact23",
+    "Apatosaurus lived around 150 million years ago",
+    "First dinosaur bones discovered 1677 by Robert Plot -https://www.discovery.com/science/First-Dinosaur-Fossil-Name",
+    "Dinosaurs extinct about 65 million years ago -https://www.kids-dinosaurs.com/dinosaur-extinction.html",
+    "The smallest meat eating dinosaurs are Hesperonychus." + imgs.dino2 + "Picture By Nobu Tamura (http://spinops.blogspot.com)",
+    "Allosaurus weight 4 tons." + imgs.dino3 + "By Fred Wierum - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=47577505",
+    "Brachiosaurus name means 'arm-reptiles'" + imgs.dino4 + "-Info https://www.kids-dinosaurs.com/brachiosaurus.html. Picture: By Богданов dmitrchel@mail.ru",
+    "Bone of a Ceratosaurus." + imgs.dino5,
+    "Bone of a Triceratops" + imgs.dino6,
+    "Joke: What did the dinosaur say when the volacano exploded?.....Answer: Have a Lava-ly day!",
+    "Heterodontonsaurus" + imgs.dino8 + "Picture: By FunkMonk (Michael B. H.) - Own work, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=44921379",
+    "Heterodontonsaurus fossil" + imgs.dino7,
     "fact24",
     "fact25",
     "fact26",
@@ -51,7 +58,6 @@ let tdData= "";
 //CALENDAR
 //displaying month and year 
 $("h2").html("December " + year) 
-
 //generating dates
 for(let i = -1; i <= 33; i++){
     getDays = i;
@@ -98,7 +104,7 @@ $("td").on("click",function(){
   
     //check if what is clicked matched today's date
     //current date 
-    if(selectDate === currentDate.format("MM/D/YYYY")){
+    if(selectDate == currentDate.format("MM/D/YYYY")){
         $(".msgModal").modal();
 
         //show match getNum to match index-1
@@ -110,7 +116,7 @@ $("td").on("click",function(){
         $(".msgModal").modal();
         $("#dinoFactHolder").html(dinosaurFacts[getNum-1]);
     }
-    //for areas with no dates/num on it  
+    // //for areas with no dates/num on it  
     else if(getNum == ""){
         $(".msgModal").modal();
         $("#dinoFactHolder").html("<p>No date here</p>");
